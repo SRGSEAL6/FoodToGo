1e314c1167b4dd7d07e686674b7ce7d56e884dd1
Javier Mendiola <javier362@revature.net>
2022-06-30 16:52:09 -0400

    Used Spring to create the project

---
+++ FoodtoGo/src/main/java/com/example/FoodtoGo/FoodtoGoApplication.java
@@ -0,0 +1,13 @@
+package com.example.FoodtoGo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodtoGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodtoGoApplication.class, args);
+	}
+
+}
---
+++ FoodtoGo/src/test/java/com/example/FoodtoGo/FoodtoGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FoodtoGo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodtoGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
