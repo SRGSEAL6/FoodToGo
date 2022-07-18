bf86d3615840c6f26b0c1c2a8016e543044ecd0d
JustMcAlpin <64049837+JustMcAlpin@users.noreply.github.com>
2022-07-18 13:38:23 -0500

    basic ui

--- FoodToGo-Front-End/Customer-P2/README.md
+++ FoodToGo-Front-End/Customer-P2/README.md
@@ -1,10 +1,10 @@
-# CustomerP2
+# AddToCart
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.0.5.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.0.4.
 
 ## Development server
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
+Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
 
 ## Code scaffolding
 
--- FoodToGo-Front-End/Customer-P2/src/app/app.component.spec.ts
+++ FoodToGo-Front-End/Customer-P2/src/app/app.component.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
       declarations: [
         AppComponent
       ],
@@ -16,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Customer-P2'`, () => {
+  it(`title should be Customer-P2'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('Customer-P2');
   });
 
-  it('should render title', () => {
+  it('render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Customer-P2 app is running!');
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('Customer-P2 working');
   });
 });
